72ed33d4bc8c28b08857bd4d1633950c3dc359b0
denta666 <skaterset@gmail.com>
2026-07-01 17:44:47 +0700

    debug auth router import

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,24 @@
 import { Router } from 'express';
+import contactRoutes from './contact.routes';
+import reviewRoutes from './review.routes';
+import authRoutes from './auth.routes';
+import menuRoutes from './menu.routes';
+import galleryRoutes from './gallery.routes';
 
-console.log("AUTH ROUTES LOADED");
+console.log("ROUTES INDEX LOADED");
+console.log("AUTH ROUTES IMPORT:", authRoutes);
 
 const router = Router();
 
-router.post('/login', (_req, res) => {
-  res.json({ message: 'LOGIN ROUTE OK' });
+router.get('/test-route', (_req, res) => {
+  res.json({ message: 'routes/index OK' });
 });
+
+router.use('/auth', authRoutes);
+
+router.use('/contact', contactRoutes);
+router.use('/reviews', reviewRoutes);
+router.use('/menus', menuRoutes);
+router.use('/galleries', galleryRoutes);
 
 export default router;
