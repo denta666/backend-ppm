271abb400096e05e324b43c9713b6ecd433d63c6
denta666 <skaterset@gmail.com>
2026-07-01 17:40:28 +0700

    test auth route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,22 +1,11 @@
 import { Router } from 'express';
-import contactRoutes from './contact.routes';
-import reviewRoutes from './review.routes';
-import authRoutes from './auth.routes';
-import menuRoutes from './menu.routes';
-import galleryRoutes from './gallery.routes';
 
-console.log("ROUTES INDEX LOADED");
+console.log("AUTH ROUTES LOADED");
 
 const router = Router();
 
-router.get('/test-route', (_req, res) => {
-  res.json({ message: 'routes/index OK' });
+router.post('/login', (_req, res) => {
+  res.json({ message: 'LOGIN ROUTE OK' });
 });
-
-router.use('/contact', contactRoutes);
-router.use('/reviews', reviewRoutes);
-router.use('/auth', authRoutes);
-router.use('/menus', menuRoutes);
-router.use('/galleries', galleryRoutes);
 
 export default router;
